779f961c77c89a7416985f5a7d43dbff913b1b4c
clark15b <clark15b@d1c30a9e-28cd-11de-a435-ffa8d773f76a>
2010-11-15 15:53:09 +0000

    HTTP proxy for radio (for PS3), playlist fixes (for proxy), XBox 360 fixes ('EXT:', 'X-User-Agent: redsonic')

--- trunk/pshare/tmpl.h
+++ trunk/pshare/tmpl.h
@@ -7,7 +7,7 @@ namespace tmpl
 {
     int print_to_xml(const char* s,FILE* fp);
     int print_to_xml2(const char* s,FILE* fp);
-    int get_file(const char* filename,FILE* dst,int tmpl,const char* date,const char* device_name);
+    int get_file(const char* filename,FILE* dst,int tmpl,const char* date,const char* device_name,const char* extra_hdrs);
 }
 
 
